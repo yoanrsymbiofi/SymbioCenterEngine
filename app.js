9483d1f942b022f3e7327061b207022ff5dd6ad9
yoanr.symbiofi <yoanr@symbiofi.com>
2017-10-14 10:34:06 +0200

    nouvelle authentification

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ var child_process = require('child_process');
 var express = require('express');
 var app = express();
 var server = http.createServer(app);
-var jwt = require('jsonwebtoken');
-var socketioJwt = require('socketio-jwt');
 var sio = require('socket.io').listen(server);
-var jwtSecret = 'BPOQlX9NnTnw7a8iQGNDUHM2RWAC8U1';
 var bodyParser = require('body-parser');
 var userSockets = [];
 var MongoClient = require("mongodb").MongoClient;
@@ -19,18 +16,7 @@ MongoClient.connect("mongodb://localhost/symbiocenterdb", function (error, db) {
 
 app.set('view engine', 'ejs');
 
-app.get('/login', function (req, res) {
-    symbioDb.collection("users").find({"email": req.query.username, "password": req.query.password}).toArray(function (error, user) {
-        if (error)
-            console.log(error);
-        if (user.length > 0) {
-            var token = jwt.sign(user[0], jwtSecret, {expiresIn: 60 * 60 * 24});
-            res.json({token: token});
-        } else {
-            res.send(401, 'Invalid user');
-        }
-    });
-}).get('/', function (req, res) {
+app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.render('index.ejs', {title: "SymbioCenter Web"});
 }).use(function (req, res, next) {
@@ -38,29 +24,46 @@ app.get('/login', function (req, res) {
     res.status(404).send('Page introuvable !');
 });
 
-sio.set('authorization', socketioJwt.authorize({
-    secret: jwtSecret,
-    handshake: true
-}));
+require('socketio-auth')(sio, {
+    authenticate: authenticate,
+    disconnect: disconnect,
+    timeout: 1000
+});
+
+function authenticate(socket, data, callback) {
+    symbioDb.collection("users").find({"email": data.username, "password": data.password}).toArray(function (err, user) {
+        user = user[0];
+        if (err || !user) {
+            return callback(new Error("User not found"));
+        } else {
+            socket.client.user = user;
+            console.log(socket.client.user.email + " connected");
+            if (userSockets[user._id] == undefined)
+                userSockets[user._id] = [];
+            userSockets[user._id].push(socket.id);
+        }
+        return callback(null, user.password == data.password);
+    });
+}
+
+function disconnect(socket) {
+    console.log(socket.client.user.email + ' disconnected');
+}
 
 sio.sockets.on('connection', function (socket) {
-    if (userSockets[socket.client.request.decoded_token._id] == undefined)
-        userSockets[socket.client.request.decoded_token._id] = [];
-    userSockets[socket.client.request.decoded_token._id].push(socket.id);
     socket.on("signal", function (signal) {
         child_process.exec('algo/algo ' + signal, function (error, stdout, stderr) {
             cc = JSON.parse(stdout);
-            cc.user_id = socket.client.request.decoded_token._id;
+            cc.user_id = socket.client.user._id;
             symbioDb.collection("biofeedbacks").insert(cc, null, function (error, results) {
                 if (error)
                     console.log(error);
             });
-            for (var i in userSockets[socket.client.request.decoded_token._id])
-                if (sio.sockets.connected[userSockets[socket.client.request.decoded_token._id][i]] != undefined)
-                    sio.sockets.connected[userSockets[socket.client.request.decoded_token._id][i]].emit('signal-received', cc);
+            for (var i in userSockets[socket.client.user._id])
+                if (sio.sockets.connected[userSockets[socket.client.user._id][i]] != undefined)
+                    sio.sockets.connected[userSockets[socket.client.user._id][i]].emit('signal-received', cc);
         });
     });
-
 });
 
 server.listen(8080, function () {
