540cfe5a70a2788a9cc4419dca4671312c797f72
yoanr.symbiofi <yoanr@symbiofi.com>
2018-02-05 12:26:06 +0100

    etzt

--- app.js
+++ app.js
@@ -132,5 +132,5 @@ function sendSignalTest() {
 }
 
 server.listen(8080, function () {
-    console.log('listening on http://localhost:8069');
+    console.log('listening on http://localhost:8080');
 });
