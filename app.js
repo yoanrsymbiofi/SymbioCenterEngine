c316148e2ea1226b0ae7d36833767c3206b053ac
yoanr.symbiofi <yoanr@symbiofi.com>
2018-02-05 11:22:48 +0100

    cc moyenne

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res) {
     res.render('index.ejs', {title: "SymbioCenter Web"});
 }).get('/login', function (req, res) {
     res.setHeader('Content-Type', 'text/html');
-    Parse.initialize('ilYgUXld1YqqqMvfpqRk59OqXt6MAKd8jis9oLhg','bylx336ZKZ4fQYnClEQ4TrTxMmkn1BhWYK07fgkP', 'myMasterKey');
+    Parse.initialize('ilYgUXld1YqqqMvfpqRk59OqXt6MAKd8jis9oLhg', 'bylx336ZKZ4fQYnClEQ4TrTxMmkn1BhWYK07fgkP', 'myMasterKey');
     Parse.User.logIn(req.query.username, req.query.password, {
         success: function (user) {
             res.send("ok");
@@ -119,17 +119,17 @@ sio.sockets.on('connection', function (socket) {
 var rrGenTab = [832, 832, 784, 788, 772, 776, 752, 800, 776, 780, 800, 776, 720, 720, 752, 768, 748, 732, 728, 736, 772, 756, 748, 748, 764, 788, 800, 800, 764, 776, 808, 824, 828, 800, 828, 840, 848, 840, 812, 808, 796, 776, 804, 880, 868, 812, 784, 804, 808, 812, 812, 800, 792, 724, 736, 748, 716, 696, 732, 808, 952, 940, 884, 880, 872, 864, 848, 812, 764, 732, 712, 740, 816, 924, 916, 884, 904, 896, 892, 876, 828, 776, 740, 732, 740, 808, 904, 892, 872, 896, 884, 880, 852, 804, 764, 732, 732, 752, 840, 892, 896, 880, 880, 856, 840, 800, 772, 732, 724, 728, 800, 892, 912, 912, 908, 896, 880, 848, 800, 752, 736, 740, 772, 872, 892, 868, 844, 783, 756, 729, 736, 718, 694, 668, 784, 808, 808, 800, 780, 764, 736, 732, 716, 716, 732, 748, 760, 756, 752, 756, 756, 760, 760, 756, 772, 780, 796, 748, 724, 732, 764, 776, 744, 792, 780, 772, 780];
 var _i = 0;
 //sendSignalTest();
-//function sendSignalTest() {
-//    if (rrGenTab[_i] == undefined)
-//        _i = 0;
-//    rr = rrGenTab[_i];
-//    _i++;
-//    console.log("yoanr@symbiofi.com send: " + rr);
-//    for (var i in userSockets["yoanr@symbiofi.com"])
-//        if (sio.sockets.connected[userSockets["yoanr@symbiofi.com"][i]] != undefined)
-//            sio.sockets.connected[userSockets["yoanr@symbiofi.com"][i]].emit('rr', rr);
-//    setTimeout(sendSignalTest, rr);
-//}
+function sendSignalTest() {
+    if (rrGenTab[_i] == undefined)
+        _i = 0;
+    rr = rrGenTab[_i];
+    _i++;
+    console.log("ymouster@symbiofi.com send: " + rr);
+    for (var i in userSockets["ymouster@symbiofi.com"])
+        if (sio.sockets.connected[userSockets["ymouster@symbiofi.com"][i]] != undefined)
+            sio.sockets.connected[userSockets["ymouster@symbiofi.com"][i]].emit('rr', rr);
+    setTimeout(sendSignalTest, rr);
+}
 
 server.listen(8080, function () {
     console.log('listening on http://localhost:8069');
